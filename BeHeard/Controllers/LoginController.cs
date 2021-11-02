227cce61db244130084cf27873f58b5aee49a7be
albertgalang <albrtglng@gmail.com>
2021-11-01 21:23:17 -0700

    Be.Heard redux; initial commit

---
+++ BeHeard/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeHeard.Models;
+
+namespace BeHeard.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new LoginModel
+            {
+                message = "hi there!"
+            };
+            return View(model);
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
---
+++ BeHeard/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeHeard.Models
+{
+    public class LoginModel
+    {
+        public string message { get; set; }
+    }
+}
