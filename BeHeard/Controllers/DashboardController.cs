4fb3a23a1e5c590cf787e813b089cd0c58c8c843
hkfrtyseven <facque3@gmail.com>
2021-11-17 15:10:32 -0800

    Simple test comment

--- BeHeard/Controllers/DashboardController.cs
+++ BeHeard/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//  Comment
 namespace BeHeard.Controllers
 {
     public class DashboardController : Controller
