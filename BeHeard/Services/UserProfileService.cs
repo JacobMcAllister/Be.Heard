1290bd2c3d12e2cc7769a68b4434f8b3425eae10
albertgalang <albrtglng@gmail.com>
2021-12-01 12:27:24 -0800

    Settings and UserProfile services

--- BeHeard/Services/UserProfileService.cs
+++ BeHeard/Services/UserProfileService.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using BeHeard.Application;
+using BeHeard.Application.Models;
+using BeHeard.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BeHeard.Services
 {
-    public class UserProfileService
+    public class UserProfileService : IUserProfileService
     {
+        // private readonly BeHeardContext _context;
+        private readonly BeHeardContextManager _contextManager;
+
+        public UserProfileService(BeHeardContext context)
+        {
+            _contextManager = new BeHeardContextManager(context);
+        }
+
+        public UserProfile GetUserProfile(Guid userId)
+        {
+            return _contextManager.UserProfileRepository.Get(userId);
+        }
+
+        public UserProfile GetUserProfile(User user)
+        {
+            return _contextManager.UserProfileRepository.GetUserProfileByUser(user);
+        }
     }
 }
