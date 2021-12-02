d447b7dab32206c7381c5b85c4edb1f7f3a8e9b3
albertgalang <albrtglng@gmail.com>
2021-12-01 23:43:35 -0800

    Migration: added user into the settings table

--- BeHeard/Migrations/20211201205259_AddUserInSettings.Designer.cs
+++ BeHeard/Migrations/20211201205259_AddUserInSettings.Designer.cs
@@ -4,14 +4,16 @@ using BeHeard.Application;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BeHeard.Migrations
 {
     [DbContext(typeof(BeHeardContext))]
-    partial class BeHeardContextModelSnapshot : ModelSnapshot
+    [Migration("20211201205259_AddUserInSettings")]
+    partial class AddUserInSettings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
