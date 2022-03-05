51dcd38dd37e5c109bae46c6d7fb9648780836ef
albertgalang <albrtglng@gmail.com>
2022-03-04 20:08:43 -0800

    Exercise view model

---
+++ BeHeard/Models/ExerciseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeHeard.Application.Models;
+
+namespace BeHeard.Models
+{
+    public class ExerciseViewModel
+    {
+        public User User { get; set; }
+        public IEnumerable<ActivityResult> ActivityResults { get; set; }
+        public string FullName => $"{User.FirstName} {User.LastName}";
+    }
+}
