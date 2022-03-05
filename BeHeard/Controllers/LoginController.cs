4a293cc85abae10126287f17a5f5147c9e0a9d33
albertgalang <albrtglng@gmail.com>
2022-03-04 20:18:32 -0800

    Removed test values for new registrations

--- BeHeard/Controllers/LoginController.cs
+++ BeHeard/Controllers/LoginController.cs
@@ -54,35 +54,9 @@ namespace BeHeard.Controllers
                 new Claim(ClaimTypes.Role, role)
             };
 
-            //var thisUser = _beHeardContextManager.UserRepository.GetUserByUsername(user.Username);
-            //var userProfile = _beHeardContextManager.UserProfileRepository.GetUserProfileByUser(thisUser);
-
-            //HttpContext.Session.SetString("FirstName", userProfile.User.FirstName);
-            //HttpContext.Session.SetString("LastName", userProfile.User.LastName);
-            //HttpContext.Session.SetString("Email", userProfile.User.Email);
-            //HttpContext.Session.SetString("Username", userProfile.User.Username);
-
-
-            //SessionModel thisSession = new SessionModel()
-            //{
-            //    FirstName = HttpContext.Session.GetString("FirstName"),
-            //    LastName = HttpContext.Session.GetString("LastName"),
-            //    FullName = HttpContext.Session.GetString("FirstName") + HttpContext.Session.GetString("LastName"),
-            //    Email = HttpContext.Session.GetString("Email"),
-            //    Username = HttpContext.Session.GetString("Username"),
-            //    Phone = " ",
-            //    City = " ",
-            //    Street = " ",
-            //    ZipCode = " ",
-            //    Age = " ",
-            //    Gender = 1
-            //};
-   
-            //HttpContext.Session.SetObjectAsJson("thisSession", thisSession);
-
-            var _user = _beHeardContextManager.UserRepository.GetUserByUsername(user.Username);
+            var authenticatedUser = _beHeardContextManager.UserRepository.GetUserByUsername(user.Username);
             var service = new SessionService(HttpContext);
-            service.Create(_user).Save();
+            service.Create(authenticatedUser).Save();
 
             // NOTE: implement refresh tokens
             var authenticationResult = _authentication.GenerateTokens(user.Username, claims, DateTime.Now);
@@ -107,78 +81,39 @@ namespace BeHeard.Controllers
         [HttpPost]
         public IActionResult RegisterAccount(User user, int termCheck)
         {
-
-
-            //var home = $"{this.Request.Scheme}://{this.Request.Host}";
-            //if (_userService.IsAnExistingUser(user.Username, user.Email)) {
-            //    TempData["Error"] = "Sorry, that 'Username' or 'Email' is already used.";
-            //    return View("Registration");
-            //}
-            //if (termCheck == 0)
+            //var subscription = new Subscription
             //{
-            //    Response.Redirect(home);
-            //}
-
-            //var newAccount = new User
-            //{
-            //    Username = user.Username,
-            //    Password = user.Password,
-
-            //    FirstName = user.FirstName,
-            //    LastName = user.LastName,RedirectToAction("Login", user);
-            //    Email = user.Email,
-            //    Gender = user.Gender,
-            //};
-            //var userSettings = new Settings
-            //{
-            //    User = newAccount
+            //    Type = SubscriptionType.Paid,
             //};
 
-            //var userProfile = new UserProfile
+            //var preferences = new Preferences
             //{
-            //    User =newAccount,
-            //    Settings = userSettings,
-            //    ActivityResults = new List<ActivityResult>()
-                
+            //    ColorBlindMode = false,
+            //    DarkMode = true,
+            //    TextToSpeech = false,
             //};
 
-            //_beHeardContextManager.UserProfileRepository.Add(userProfile);
-            //_beHeardContextManager.SaveChanges();
 
-            var subscription = new Subscription
-            {
-                Type = SubscriptionType.Paid,
-            };
+            //var settings = new Settings
+            //{
+            //    MasterVolume = 100,
+            //    Preferences = preferences,
+            //    Subscription = subscription,
+            //    User = user,
+            //};
+            //var profile = new UserProfile
+            //{
+            //    Settings = settings,
+            //    User = user,
+            //};
 
-            var preferences = new Preferences
-            {
-                ColorBlindMode = false,
-                DarkMode = true,
-                TextToSpeech = false,
-            };
-
-
-            var settings = new Settings
-            {
-                MasterVolume = 100,
-                Preferences = preferences,
-                Subscription = subscription,
-                User = user,
-            };
-            var profile = new UserProfile
-            {
-                Settings = settings,
-                User = user,
-            };
-
-            user.Settings = settings;
-            user.Profile = profile;
+            //user.Settings = settings;
+            //user.Profile = profile;
 
             try
             {
                 _beHeardContextManager.UserRepository.Add(user);
                 _beHeardContextManager.SaveChanges();
-                // return Redirect("~/?NewUser=1");
                 return Login(user, true);
             }
             catch
