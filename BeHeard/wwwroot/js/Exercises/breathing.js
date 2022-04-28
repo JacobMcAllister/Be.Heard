3751f2cc791b2044f430055c9fbce9bc9488de09
hkfrtyseven <facque3@gmail.com>
2022-04-27 18:40:09 -0700

    Update to breathing animation

--- BeHeard/wwwroot/js/Exercises/breathing.js
+++ BeHeard/wwwroot/js/Exercises/breathing.js
@@ -1,14 +1,16 @@
 ﻿var timer = 14;
-var timer_sec = 14000;
+var anim_diff = 1;
 var breatheIn = 7;
-var breatheIn_sec = 7000;
+var breatheIn_sec = 7500;
 var breatheOut_sec = 7000;
 var breatheOut = 7;
 var difficulty = null;
 var diff_val = null;
 var downloadTimer = null;
+var breathing_timer = null;
 var container = document.getElementById("breathing_anim");
 var text = document.getElementById("text");
+var pointer_container = document.getElementById("rotating_ball");
 
 // DB Fields
 var SentenceSet = -1;
@@ -34,7 +36,7 @@ function UpdateDB() {
     })
 }
 
-document.getElementById("startButton").onclick = function () { start_timer() };
+document.getElementById("startButton").onclick = function () { clock(); breathe_animation(breatheIn_sec, anim_diff); };
 
 function defineTime(menu_in, menu_out) {
     document.getElementById("breathin").innerHTML = menu_in;
@@ -42,10 +44,11 @@ function defineTime(menu_in, menu_out) {
 }
 
 function defineTimeonload() {
+    text.innerHTML = " - ";
     document.getElementById("breathin").innerHTML = breatheIn;
     document.getElementById("breathout").innerHTML = breatheOut;
     document.getElementById("timer").innerHTML = "Relax";
-    document.getElementById("time_remaining").innerHTML = '-';
+    document.getElementById("time_remaining").innerHTML = " - ";
 }
 
 function clock() {
@@ -61,7 +64,12 @@ function clock() {
         }
         else {
             UpdateDB();
+            //pointer_container.className = 'pointer-container stop';
+            //pointer_container.removeClass('pointer-container ');
+            text.innerHTML = "Relax";
+            pointer_container.className = 'pointer-container';
             clearInterval(downloadTimer);
+            clearInterval(breathing_timer);
             document.getElementById("time_remaining").innerHTML = '-';
             document.getElementById("timer").innerHTML = "Relax";
         }
@@ -70,26 +78,6 @@ function clock() {
     }
 
     var downloadTimer = setInterval(timeDetails, 1000);
-}
-
-function start_timer() {
-    /*
-    switch (diff_level) {
-        case 1:
-            clock();
-            break;
-        case 2:
-            clock();
-            break;
-        case 3:
-            clock();
-            break;
-        case 4:
-            clock();
-            break;
-    }
-    */
-    clock();
 }
 
 function diff_alert(case_num) {
@@ -121,25 +109,33 @@ function difficulty_dropdown() {
         switch (true) {
             case (value == 'Easy'):
                 timer = 14;
+                anim_diff = 1;
                 breatheIn = 7;
+                breatheIn_sec = 7500;
                 breatheOut = 7;
                 diff_alert(1);
                 break;
             case (value == 'Medium'):
                 timer = 18;
+                anim_diff = 2;
                 breatheIn = 9;
+                breatheIn_sec = 9500;
                 breatheOut = 9;
                 diff_alert(2);
                 break;
             case (value == 'Hard'):
                 timer = 22;
+                anim_diff = 3;
                 breatheIn = 11;
+                breatheIn_sec = 11500;
                 breatheOut = 11;
                 diff_alert(3);
                 break;
             case (value == 'Extreme'):
                 timer = 30;
+                anim_diff = 4;
                 breatheIn = 15;
+                breatheIn_sec = 15500;
                 breatheOut = 15;
                 diff_alert(4);
                 break;
@@ -149,19 +145,61 @@ function difficulty_dropdown() {
     alter_difficulty(diff_value);
 }
 
-breathe_animation();
+function breathe_animation(breathing, anim) {
+    console.log(breathing);
+    console.log(anim);
 
-function breathe_animation() {
     text.innerHTML = "Breathe In";
-    container.className = 'container grow';
 
-    setTimeout(() => {
-        text.innerHTML = "Breathe Out";
-        container.className = 'container shrink';
+    switch (anim) {
+        case 1:
+            pointer_container.className = 'pointer-container rotate7';
+            container.className = 'container grow7';
+            breathing_timer = setTimeout(() => {
+                text.innerHTML = "Breathe Out";
+                container.className = 'container shrink7';
 
-    }, breatheIn_sec)
+            }, breathing)
+            break;
+        case 2:
+            pointer_container.className = 'pointer-container rotate9';
+            container.className = 'container grow9';
+            breathing_timer = setTimeout(() => {
+                text.innerHTML = "Breathe Out";
+                container.className = 'container shrink9';
+
+            }, breathing)
+            break;
+        case 3:
+            pointer_container.className = 'pointer-container rotat11';
+            container.className = 'container grow11';
+            breathing_timer = setTimeout(() => {
+                text.innerHTML = "Breathe Out";
+                container.className = 'container shrink11';
+
+            }, breathing)
+            break;
+        case 4:
+            pointer_container.className = 'pointer-container rotate15';
+            container.className = 'container grow15';
+            breathing_timer = setTimeout(() => {
+                text.innerHTML = "Breathe Out";
+                container.className = 'container shrink15';
+
+            }, breathing)
+            break;
+    }
 }
 
-setInterval(breathe_animation, timer_sec)
-
 //animation: rotate 14s linear forwards infinite;
+/*
+ *  #rotating_ball.stop {
+        position:absolute;
+        top: -30px;
+        left: 140px;
+        width: 20px;
+        height: 180px;
+        transform-origin: bottom center;
+        animation: rotate 15s linear forwards 0;
+    }
+*/
